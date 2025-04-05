4cbcdc1144333d228c6cc79ab054c0052165fd86
eduthiago <eduthiago2@gmail.com>
2025-04-05 16:37:13 -0300

    Atualiza conteúdo do App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eduardo Monteiro - Cientista de Dados
+          <br />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React
-        </a>
+        className="App-link" href="https://reactjs.org" target="_blank"
+        rel="noopener noreferrer" Aprenda React
       </header>
     </div>
   );
