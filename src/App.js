1c3e35b6ed48117776dea472071eb981b2164dd4
eduthiago <eduthiago2@gmail.com>
2025-04-05 16:41:11 -0300

    Atualiza conteúdo do App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,38 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Eduardo Monteiro - Cientista de Dados
-          <br />
-        </p>
-        className="App-link" href="https://reactjs.org" target="_blank"
-        rel="noopener noreferrer" Aprenda React
-      </header>
+    <div
+      style={{
+        padding: "40px",
+        fontFamily: "Arial",
+        maxWidth: "600px",
+        margin: "auto",
+      }}
+    >
+      <h1>Eduardo Thiago</h1>
+      <h2>Desenvolvedor Front-End</h2>
+
+      <p>
+        Sou um desenvolvedor iniciante apaixonado por tecnologia, buscando minha
+        primeira oportunidade na área. Estudando React, JavaScript, e outras
+        ferramentas modernas para desenvolvimento web.
+      </p>
+
+      <h3>Redes Sociais</h3>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/eduthiago"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="mailto:eduthiago@gmail.com">E-mail</a>
+        </li>
+      </ul>
     </div>
   );
 }
